07301b9117dbbae5f95369a156b71b55833de9ef
Aarush Sharma <aarush.sharma6@gmail.com>
2022-03-13 11:14:16 +0530

    styling in progress

--- area.js
+++ area.js
@@ -5,18 +5,18 @@ const button = document.querySelector("#calc-btn");
 
 const area = document.querySelector("#output");
 
-function calculateArea(){
+function calculateArea() {
 
-    if ((Number(triBase.value)) < 0){
+    if ((Number(triBase.value)) < 0) {
         alert("You are dumb cause base negative kaise?");
         return;
     }
 
-    if  ((Number(triHt.value))<0) { 
+    if ((Number(triHt.value)) < 0) {
         alert("You are dumb cause height negative kaise?");
         return;
     }
-    
+
     areaOfTri = 0.5 * Number(triBase.value) * Number(triHt.value);
     //console.log(areaOfTri);
     area.innerText = "The area is " + areaOfTri;
